6de6bcf6c2ea899359dd79169648d0bf3c829b69
MarthaRivas <groenezeeriv@gmail.com>
2018-09-27 14:16:29 +0200

    agregando redux-devtools-dependencys

--- nieuwcourse/src/nieuwStudent.js
+++
@@ -1,5 +0,0 @@
-function nieuwStudent (name,course,enroll,quit){
-    this.name = name;
-    this.course = {course : 'biology', 'science'};
-    this.enroll = enroll
-}
--- nieuwcourse/src/store.js
+++ nieuwcourse/src/store.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // state
 
@@ -18,79 +19,86 @@ const initialState = {
 
 // actions
 
-export function addStudent(Student) {
-  return { type: "ADD_STUDENT", payload: Student }
+export function addStudent(student) {
+  return { type: "ADD_STUDENT", payload: student }
 }
 
-export function removeStudent(quitstudent) {
-  return { type: "REMOVE_STUDENT", payload: quitstudent }
+export function removeStudent(student) {
+  return { type: "REMOVE_STUDENT", payload: student }
 }
 
 export function addCourse(course) {
-  return { type: "ADD_COURSE", payload: course, }
+  return { type: "ADD_COURSE", payload: course }
 }
 
 export function removeCourse(course) {
   return { type: "REMOVE_COURSE", payload: course }
 }
 
-export function enroll(Student, course) {
-  return { type: "ENROLL", payload: Student.course }
+export function enroll(student, course) {
+  return { type: "ENROLL", payload: { student, course } }
 }
 
-export function quit(Student, course) {
-  return { type: "QUIT", payload: Student.course}
+export function quit(student, course) {
+  return { type: "QUIT", payload: { student, course } }
 }
 
 
 // reducer
 
 function reducer(state, action) {
-  if (action.type === "ADD_STUDENT")
- 
-  
-  return {...state,}
-}
 
-{
-  if (action.type === "REMOVE_STUDENT")
- 
-  
-  return {...state,}
-}
+  if (action.type === "ADD_STUDENT") {
+    return {
+      ...state,
+      students: [...state.students, action.payload],
+    }
+  }
 
-{
-  if (action.type === "ADD_COURSE")
- 
-  
-  return {...state,}
-}
+  if (action.type === "REMOVE_STUDENT") {
+    return {
+      ...state,
+      students: state.students.filter(s => s !== action.payload),
+      enrollments: state.enrollments.filter(e => e.student !== action.payload)
+    }
+  }
 
-{
-  if (action.type === "REMOVE_COURSE")
- 
-  
-  return {...state,}
-}
+  if (action.type === "ADD_COURSE") {
+    return {
+      ...state,
+      courses: [...state.courses, action.payload],
+    }
+  }
 
-{
-  if (action.type === "ENROLL")
- 
-  
-  return {...state,
-  enrollments: {}}
-}
+  if (action.type === "REMOVE_COURSE") {
+    return {
+      ...state,
+      courses: state.courses.filter(c => c !== action.payload),
+      enrollments: state.enrollments.filter(e => e.course !== action.payload)
+    }
+  }
 
-{
-  if (action.type === "QUIT")
- 
-  
-  return {...state,}
-}
+  if (action.type === "ENROLL") {
+    return {
+      ...state,
+      enrollments: [...state.enrollments, action.payload]
+    }
+  }
 
+  if (action.type === "QUIT") {
+    const { student, course } = action.payload
+    return {
+      ...state,
+      enrollments: state.enrollments.filter(e =>
+        !(e.student === student && e.course === course))
+    }
+  }
+
+  return state
+}
 
 // store
 
-const store = createStore(reducer, initialState)
+const store = createStore(reducer, initialState, composeWithDevTools());
 
 export default store
